324cf2555a9d6f857dc44146661e6446836bf912
Wayne Chen <ichenwin@icloud.com>
2016-10-26 18:21:43 +0800

    Initial Commit

---
+++ ClipBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ClipBoard
+//
+//  Created by wayne on 16/10/26.
+//  Copyright © 2016年 wayne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ClipBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ClipBoard
+//
+//  Created by wayne on 16/10/26.
+//  Copyright © 2016年 wayne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
